cb34bc9c06b2ff83e9fe408761b943a00c97ad8f
Minh Nhan <nmnhan2641999@gmail.com>
2024-02-26 16:21:55 +0700

    config,connect to db

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,9 +1,22 @@
+import { json } from "body-parser";
+import db from "../models/index"
+
 
 let getHomePage = (req, res) => {
     return res.render('homepage.ejs');
 }
-let getAbout = (req, res) => {
-    return res.render('test/about.ejs')
+let getAbout = async (req, res) => {
+    try {
+        let data = await db.User.findAll();
+        return res.render('test/about.ejs', {
+            data: JSON.stringify(data)
+        })
+
+
+    } catch (error) {
+        console.log(error);
+    }
+
 }
 
 //object:{
